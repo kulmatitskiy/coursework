36f63a44ddcdd28c3304fe89e3a4112a20d4f479
Aui Mendoza <auimendoza@gmail.com>
2016-03-15 18:13:41 +0000

    modify window slide from 10 to 30 remove solr reload core add commitWithin=5000 to solr post request add replace for ':'

--- readstream.py
+++ readstream.py
@@ -34,7 +34,7 @@ def getShow(line):
   tsent = 0
   for word in words:
     try:
-      show = shows[word.replace("#","").replace("@","")].encode('ascii')
+      show = shows[word.replace("#","").replace("@","").replace(":","")].encode('ascii')
       return(show)
     except:
       None
@@ -44,14 +44,14 @@ def getText(line):
   return((j["created_at"], j["id"], j["text"]))
 
 def postToSolr(t, rdd):
-  solr_url = "http://auisbigdatabox.cloudapp.net:8983/solr/project/update"
+  solr_url = "http://auisbigdatabox.cloudapp.net:8983/solr/project/update?commitWithin=5000"
   d = json.dumps(rdd.map(lambda x: x).collect())
   print(d)
   h = {'content-type': 'application/json'}
   r = requests.post(solr_url, data=d, headers=h)
   print(r.status_code)
-  r2 = requests.get("http://auisbigdatabox.cloudapp.net:8983/solr/admin/cores?action=RELOAD&core=project_shard1_replica1")
-  print(r2.status_code)
+  #r2 = requests.get("http://auisbigdatabox.cloudapp.net:8983/solr/admin/cores?action=RELOAD&core=project_shard1_replica1")
+  #print(r2.status_code)
 
 
 sc = SparkContext()
@@ -63,7 +63,7 @@ shows = dict(sc.textFile("/data/shows.txt").map(lambda line: (line.lower().encod
 
 ssc = StreamingContext(sc, 1)
 ds = ssc.textFileStream("/project/rawdata")
-ods = ds.window(30, 10).map(lambda line: getText(line)) \
+ods = ds.window(30, 30).map(lambda line: getText(line)) \
   .map(lambda line: {"created_dt": line[0], "show_s": getShow(line[2]), "sentiment_s": getLineSentiment(line[2]), "text_t": line[2]})
 ods.foreachRDD(postToSolr)
   
